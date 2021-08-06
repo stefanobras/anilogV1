78b60697317e6a718c390bd6df5d104093576610
stefanobras <stefanobras20@gmail.com>
2021-08-05 23:44:26 -0300

    home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './img/al-logo-cube.png';
+import logoFont from './img/al-logo-font.png';
 import './App.css';
 
 function App() {
@@ -6,18 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload, Anilog Test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logoFont} className="App-logo2" alt="logo" />
       </header>
+
     </div>
   );
 }
